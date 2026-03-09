9d740aa7f6190a72f6882b414ffe0e296a310754
owongy0 <owong.oscar2@gmail.com>
2026-03-09 22:39:53 +0800

    chore: configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  // For GitHub User Pages, no basePath is needed
+  // The site will be at https://owongy0.github.io/
 };
 
 export default nextConfig;
